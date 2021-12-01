38ea9bc967ed7a4662992e225f9c6a67fd0159de
MykhayloZelisko <zelisko.mykhaylo@gmail.com>
2021-12-01 10:24:45 +0200

    fix user ngrx

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppStoreState } from './app-store/app-store.state';
+import { LoginSuccess } from './app-store/user/user.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+
+  constructor(private _store: Store<AppStoreState>, private _router: Router) {}
+
+  ngOnInit(): void {
+    const token = localStorage.getItem('token');
+    this._router.navigateByUrl('/login');
+    if (token) {
+      this._store.dispatch(new LoginSuccess(token) );
+    }
+  }
 }
